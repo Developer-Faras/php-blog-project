e0d587ded07ed295f1306d3026fff0a5cce95205
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-03-28 14:55:05 +0600

    Main Template Designed

---
+++ includes/head.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Md Faras Uddin">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+
+    <title><?php 
+      if(isset($page_title)){
+        echo $page_title;
+      }else{
+        echo 'Stand Blog';
+      }
+    ?></title>
+
+    <?php include_once('./includes/styles.php');?>
+<!--
+
+TemplateMo 551 Stand Blog
+
+https://templatemo.com/tm-551-stand-blog
+
+-->
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
